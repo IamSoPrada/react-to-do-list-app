312fe8245c3d2335fbbdc8f3a32dd09ddbd2d156
IamSoPrada <9999goldcoin@gmail.com>
2020-07-02 00:12:18 +0400

    App 'to-do-list' finished, added new function 'Filter by like'

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,7 +13,7 @@ import styled from "styled-components";
 const AppBlock = styled.div`
 
     margin: 0 auto;
-    max-width: 900px;
+    max-width: 800px;
 
 `
 
@@ -27,7 +27,8 @@ export default class App extends Component {
                 { label: "I'd like to eat", important: false, like: false, id: 2 },
                 { label: "Need to buy paper for printer", important: false, like: false, id: 3 },
             ],
-            term: ""
+            term: "",
+            filter: "all"
 
         }
         this.deleteItem = this.deleteItem.bind(this);
@@ -35,6 +36,7 @@ export default class App extends Component {
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLiked = this.onToggleLiked.bind(this);
         this.onUpdateSearch = this.onUpdateSearch.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
         this.maxId = 4;
     }
 
@@ -104,14 +106,27 @@ export default class App extends Component {
         });
     }
 
+    filterPost(items, filter){
+        if(filter === "like"){
+            return items.filter(item => item.like);
+        } else {
+            return items;
+        }
+    }
+
+    onFilterSelect(filter){
+        this.setState({filter});
+    }
+
     onUpdateSearch(term){
         this.setState({term});
     }
+
     render() {
-        const { data, term } = this.state;
+        const { data, term, filter } = this.state;
         const liked = data.filter(item => item.like).length;
         const allPosts = data.length;
-        const visiblePosts = this.searchPost(data, term);
+        const visiblePosts = this.filterPost(this.searchPost(data, term), filter); // первый аргумент здесь это item из ф-ции filterPost; filter получен из state
         return (
             <AppBlock>
                 <AppHeader
@@ -120,7 +135,9 @@ export default class App extends Component {
                 <div className="search-panel d-flex">
                     <SearchPanel 
                     onUpdateSearch = {this.onUpdateSearch}/>
-                    <PostStatusFilter />
+                    <PostStatusFilter 
+                    filter={filter}
+                    onFilterSelect={this.onFilterSelect}/>
                 </div>
                 <PostList
                     posts={visiblePosts}  // пропс откуда берем наш список дел //пропс onDelete который вызывает одноименную ф-цию
--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, {Component} from "react";
 import "./post-status-filter.css"
-const PostStatusFilter = () => {
-    return (
-        <div className="btn-group">
-            <button type="button" className="btn btn-dark">Все</button>
-            <button type="button" className="btn btn-light btn-outline-secondary">Понравилось</button>
-        </div>
-    )
+export default class PostStatusFilter extends Component {
+    constructor(props){
+        super(props);
+
+        this.buttons = [ // создаем массив наших кнопок, массив создаем для того чтобы перебирать при помощи методов массива (map, filter)
+            {
+                name : "all",  // имя фильтра по которому отфильтровываем ф-ция filterPost App.js
+                label: "Все"
+            },
+            {
+                name : "like",
+                label: "Понравилось"
+            }
+        ]
+        this.state ={
+
+        }
+    }
+    render(){
+        const buttons = this.buttons.map(({name, label})=> { // вытаскиваем(деструктуризируем) из массива buttons имя фильтра и label
+            const {filter, onFilterSelect} = this.props;
+            const active = filter === name;
+            const clazz = active ? "btn-dark" : "btn-light"
+            return(
+                <button 
+                    key={name} 
+                    type="button" 
+                    className={`btn ${clazz}`}
+                    onClick={()=> onFilterSelect(name)}>{label}</button>
+            );
+        })
+        return (
+            <div className="btn-group">
+                {buttons}
+            </div>
+        )
+    }
 }
-export default PostStatusFilter;
