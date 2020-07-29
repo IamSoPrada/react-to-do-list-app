47a561faa8ce18d9a290c1a63f4a9c2a1739589f
IamSoPrada <9999goldcoin@gmail.com>
2020-07-29 21:20:32 +0400

    Completed

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,7 +3,7 @@ import PostListItem from "../post-list-item";
 import "./post-list.css"
 
 
-const PostList = ({ posts, onDelete, onToggleImportant, onToggleLiked}) => { // posts - это пропс который хранит this.state.data данные которые берутся со страницы App, объект data, делается это для того чтобы формировать 
+const PostList = ({ posts, onDelete, onToggleImportant, onToggleLiked }) => { // posts - это пропс который хранит this.state.data данные которые берутся со страницы App, объект data, делается это для того чтобы формировать 
     //любое количество ListItems в зависимости от того что пришло с сервера (data)
 
     const elements = posts.map((item => { // перебираем с помощью метода map
@@ -14,6 +14,7 @@ const PostList = ({ posts, onDelete, onToggleImportant, onToggleLiked}) => { // 
             <li key={id} className="list-group-item">
                 <PostListItem
                     label={itemProps.label}
+                    like={itemProps.like}
                     important={itemProps.important}
                     onDelete={() => onDelete(id)}
                     onToggleImportant={() => onToggleImportant(id)}
